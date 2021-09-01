ab5f0e0bcdcd6349cbd441a75de34368be6fcebe
minhloi <jimmyloi92@yahoo.com>
2021-09-01 16:09:59 -0700

    fix: 🐛 do not verify iat

--- lib/firebase_id_token/signature.rb
+++ lib/firebase_id_token/signature.rb
@@ -29,7 +29,7 @@ module FirebaseIdToken
   class Signature
     # Pre-default JWT algorithm parameters as recommended
     # [here](https://goo.gl/uOK5Jx).
-    JWT_DEFAULTS = { algorithm: 'RS256', verify_iat: true }
+    JWT_DEFAULTS = { algorithm: 'RS256', verify_iat: false }
 
     # Returns the decoded JWT hash payload of the Firebase ID Token if the
     # signature in the token matches with one of the certificates downloaded
@@ -117,8 +117,7 @@ module FirebaseIdToken
     end
 
     def still_valid?(payload)
-      payload['exp'].to_i > Time.now.to_i &&
-      payload['iat'].to_i <= Time.now.to_i
+      payload['exp'].to_i > Time.now.to_i
     end
 
     def issuer_authorized?(payload)
